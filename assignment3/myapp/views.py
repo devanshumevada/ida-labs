3e5252d58a7b93d2a82908f631f8510d90684138
devanshumevada <devanshumevada@gmail.com>
2022-06-29 19:14:41 -0400

    lab 6 completed

--- assignment3/myapp/views.py
+++ assignment3/myapp/views.py
@@ -5,15 +5,14 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
-    return render(request, 'myapp/index0.html', {'top_list': top_list})
+    return render(request, 'myapp/index.html', {'top_list': top_list})
 
 
 def about(request):
-   return render(request, 'myapp/about0.html')
+   return render(request, 'myapp/about.html')
 
 
 def detail(request, top_no):
     topic = get_object_or_404(Topic, pk=top_no)
     courses = Course.objects.filter(topic=topic)
-
-    return render(request, 'myapp/detail0.html', {'topic': topic, 'courses': courses})   
+    return render(request, 'myapp/detail.html', {'topic': topic, 'courses': courses})   
